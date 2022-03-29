7be898474d9ffbe6474c3a4b2ea6892f8b290c97
kadlicjp <joekadlic@outlook.com>
2022-03-29 18:52:54 -0400

    - Removed Debug - Removed call to RecipeManager.getAllRecipes() from Main

--- src/main/java/com/fireside/pantry/Main.java
+++ src/main/java/com/fireside/pantry/Main.java
@@ -1,17 +1,7 @@
 package com.fireside.pantry;
 
-import com.fireside.pantry.app.UserManager;
-import com.fireside.pantry.util.objects.User;
-
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
          App.main(args);
-
-        List<User> users = UserManager.getAllUsers();
-        for (User user : users) {
-            System.out.println(user);
-        }
     }
 }
