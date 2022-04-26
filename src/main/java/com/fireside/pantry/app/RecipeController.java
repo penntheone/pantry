3c56597141d90d666b2b598668fde4e2e774b7ea
Anthony Brey <breyad@miamioh.edu>
2022-04-26 17:03:11 -0400

    - Documentation has been added to Recipe and User controllers. Do fix if needed.

--- src/main/java/com/fireside/pantry/app/RecipeController.java
+++ src/main/java/com/fireside/pantry/app/RecipeController.java
@@ -10,10 +10,20 @@ import java.util.List;
 
 public class RecipeController {
 
+    /**
+     * Retrieves the entire list of recipes
+     * @return all the recipes
+     */
     public static List<Recipe> getAllRecipes() {
         return getRecipes("CALL spGetAllRecipes()");
     }
 
+    /**
+     * Retrieves a range of recipes with a starting and end point given
+     * @param start The first recipe in the range
+     * @param end The last recipe in the range
+     * @return The list of recipes from start to end
+     */
     public static List<Recipe> getRangeOfRecipes(int start, int end) {
         List<Recipe> recipes = new ArrayList<>();
         for (int i = start; i < end + 1; i++) {
@@ -22,11 +32,21 @@ public class RecipeController {
         return recipes;
     }
 
+    /**
+     * Retrieves recipes with the specified ingredient
+     * @param ingredient the specified ingredient
+     * @return all recipes with the ingredients
+     */
     public static List<Recipe> getRecipesByIngredient(String ingredient) {
         String query = String.format("CALL spGetRecipesByIngredient('%s');", ingredient);
         return getRecipes(query);
     }
 
+    /**
+     * Retrieves the recipe with the specified ID
+     * @param recipeId The specific ID
+     * @return The recipe with that ID
+     */
     public static Recipe getRecipeByID(int recipeId) {
         String query = String.format("CALL spGetRecipeByID(%d);", recipeId);
         List<Recipe> recipes = getRecipes(query);
@@ -35,6 +55,11 @@ public class RecipeController {
         return recipes.get(0);
     }
 
+    /**
+     * Retrieves the recipe id based on the entered name
+     * @param name the entered name
+     * @return the id for the named recipe
+     */
     public static int getRecipeIDByName(String name) {
         String query = String.format("CALL spGetRecipeIDByName('%s');", name);
         List<Recipe> recipes = getRecipes(query);
@@ -43,21 +68,41 @@ public class RecipeController {
         return recipes.get(0).getId();
     }
 
+    /**
+     * Retrieves the recipes from a specified region
+     * @param region The specified region
+     * @return The recipes from that region
+     */
     public static List<Recipe> getRecipesByRegion(String region) {
         String query = String.format("CALL spGetRecipeByRegion('%s');", region);
         return getRecipes(query);
     }
 
+    /**
+     * Retrieves the recipes from a specified category
+     * @param category The specified category
+     * @return The recipes from that category
+     */
     public static List<Recipe> getRecipesByCategory(String category) {
         String query = String.format("CALL spGetRecipeByCategory('%s');", category);
         return getRecipes(query);
     }
 
+    /**
+     * Retrieves recipes based on an entered title
+     * @param title The entered title
+     * @return the recipes that match with the title
+     */
     public static List<Recipe> getRecipesByTitle(String title) {
         String query = String.format("CALL spGetRecipeByTitle('%s');", title);
         return getRecipes(query);
     }
 
+    /**
+     * Gets recipes based on a search query
+     * @param query The search query
+     * @return Search results based on query
+     */
     private static List<Recipe> getRecipes(String query) {
         try {
             List<Row> rows = new DatabaseConnector().query(query);
--- src/main/java/com/fireside/pantry/app/UserController.java
+++ src/main/java/com/fireside/pantry/app/UserController.java
@@ -10,24 +10,48 @@ import java.util.List;
 
 public class UserController {
 
+    /**
+     * Gets list of users
+     * @return All users
+     */
     public static List<User> getAllUsers() {
         return getUsers("CALL spGetAllUsers();");
     }
 
+    /**
+     * Gets a user based on a specified first name
+     * @param firstName the first name
+     * @return a user based on this first name
+     */
     public static User getUserByFirstName(String firstName) {
         return getUser(String.format("CALL spGetUserByFName('%s');", firstName));
     }
 
+    /**
+     * Gets a user based on a specified last name
+     * @param lastName the last name
+     * @return a user based on this last name
+     */
     public static User getUserByLastName(String lastName) {
         return getUser(String.format("CALL spGetUserByLName('%s');", lastName));
     }
 
+    /**
+     * Gets a user based on a specified username
+     * @param username the username
+     * @return a user based on this username
+     */
     public static User getUserByUsername(String username) {
         return getUser(String.format("CALL spGetUserByUsername('%s');", username));
     }
 
     // -------------- Private methods --------------
 
+    /**
+     * Gets a user based on a search query
+     * @param query The search query
+     * @return Search results based on query
+     */
     private static User getUser(String query) {
         try {
             List<Row> rows = new DatabaseConnector().query(query);
@@ -40,6 +64,11 @@ public class UserController {
         }
     }
 
+    /**
+     * Gets users based on a search query
+     * @param query The search query
+     * @return Search results based on query
+     */
     private static List<User> getUsers(String query) {
         try {
             List<Row> rows = new DatabaseConnector().query(query);
